751848e7961e0751e640c4e8b31dd106bd37a00d
farahnaj <farahinamdar@gmail.com>
2015-06-16 18:30:20 +0530

    first commit

---
+++ grails-app/domain/mylibsys/Book.groovy
@@ -0,0 +1,15 @@
+package mylibsys
+
+class Book {
+
+    String title;
+    String author;
+    String description;
+
+    static constraints = {
+    }
+
+    static mapping = {
+        table "Book"
+    }
+}
